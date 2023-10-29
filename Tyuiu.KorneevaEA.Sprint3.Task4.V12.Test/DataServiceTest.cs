31541df38dd5b00b0af6c8f1d08b3f2eab5a4c75
Елизавета Корнеева <kornea2005@mail.ru>
2023-10-29 17:28:39 +0500

    Task4

---
+++ Tyuiu.KorneevaEA.Sprint3.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KorneevaEA.Sprint3.Task4.V12.Lib;
+
+namespace Tyuiu.KorneevaEA.Sprint3.Task4.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(-606.317, ds.Calculate(-5, 5));
+        }
+
+    }
+}
---
+++ Tyuiu.KorneevaEA.Sprint3.Task4.V12/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KorneevaEA.Sprint3.Task4.V12.Lib;
+
+namespace Tyuiu.KorneevaEA.Sprint3.Task4.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнила: Корнеева Е.А. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*Тема: Создания итогового решения по спринту                              *");
+            Console.WriteLine("*Задание #4                                                               *");
+            Console.WriteLine("*Вариант #12                                                              *");
+            Console.WriteLine("*Выполнил: Корнеева Елизавета Андреевна | АСОиУб-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая подсчитает значение функции на отрезке x     ");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int x = -5;
+            int y = 5;
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("По моим подсчётам " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
