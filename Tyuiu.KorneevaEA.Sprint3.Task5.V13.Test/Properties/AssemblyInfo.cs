b565520f6fd671005e10fa4b72f890013ed11d20
Елизавета Корнеева <kornea2005@mail.ru>
2023-10-29 17:49:14 +0500

    Task5

---
+++ Tyuiu.KorneevaEA.Sprint3.Task5.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KorneevaEA.Sprint3.Task5.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KorneevaEA.Sprint3.Task5.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14c5dc46-1d18-4397-b245-063dfbbecc3a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KorneevaEA.Sprint3.Task5.V13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KorneevaEA.Sprint3.Task5.V13.Lib;
+
+namespace Tyuiu.KorneevaEA.Sprint3.Task5.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнила: Корнеева Е.А. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*Тема: Вложенные циклы                                                    *");
+            Console.WriteLine("*Задание #5                                                               *");
+            Console.WriteLine("*Вариант #13                                                              *");
+            Console.WriteLine("*Выполнил: Корнеева Елизавета Андреевна | АСОиУб-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму суммы ряда.                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 12;
+            Console.WriteLine("Х = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Результат: " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
